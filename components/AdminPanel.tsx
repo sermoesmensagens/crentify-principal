b400f3e38b005e8566ee9948354301b7d25c7fc3
sermoesmensagens <sermoes.mensagens@gmail.com>
2026-04-01 00:42:22 -0300

    admin

--- components/AdminPanel.tsx
+++ components/AdminPanel.tsx
@@ -271,7 +271,7 @@ const AdminPanel: React.FC = () => {
   };
 
   return (
-    <div className="h-full flex flex-col space-y-10 animate-in fade-in duration-700 pb-24">
+    <div className="flex flex-col space-y-10 animate-in fade-in duration-700 pb-24">
       <header className="flex flex-col md:flex-row justify-between items-end gap-6 border-b border-white/5 pb-8">
         <div>
           <h1 className="text-5xl font-black text-white tracking-tighter uppercase neon-text flex items-center gap-4">
